c3466546a3db342a54eb0c4b7bdb2ef674d37813
bjorkrothjm <j.m.bjorkroth@gmail.com>
2024-06-28 20:51:39 +0200

    Creating variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+// userName is a variable of type "string"
+userName = "Josh";
+userSurname = 'Gårlin'
+userTitle = `Student` // written with shift + ¿
+
+console.log(userName);
+console.log(userSurname);
+console.log(userTitle);
+
+console.log(userName, userSurname, userTitle);
+console.log(userName + userSurname + userTitle);
+console.log(userName +  " " + userSurname + " " + userTitle);
