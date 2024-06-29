5deddaf001889590239f0568f56b8186b76152e7
bjorkrothjm <j.m.bjorkroth@gmail.com>
2024-06-29 15:02:38 +0200

    Updating variables

--- variables.js
+++ variables.js
@@ -1,3 +1,5 @@
+console.log("This is variables.js")
+
 // userName is a variable of type "string"
 userName = "Josh";
 userSurname = 'Gårlin'
@@ -22,11 +24,35 @@ userName = "Marco"; // string
 {
     let userNickname = "Joshi" // assinging within local scope
 }
-console.log(userNickname); // Will give error because only defined within local scope
+// console.log(userNickname); // Will give error because only defined within local scope
 
 const userNickname =  "Joshi"// assinging with 
 console.log(userNickname); // Will NOT give error
 
-// TODO: arrays, objects
+
+// Ejercicio - Should output "2, 1" (without assigning numbers to any variable)
+a = 1;
+b = 2;
+
+/// Only change code here
+// c = 2;
+// b = a;
+// a = c;
+
+//// Solition 1 
+c = a;
+a = b;
+b = c;
+
+//// Solition 2
+// [a, b] = [b, a]
+
+/// Don't change code here 
+console.log(a, b); // should output "2, 1"
+
+
+// TODO arrays, objects
+
+
 
 
