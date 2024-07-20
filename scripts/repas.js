650e9dde55ee9dfbfe4855fd801e8cdc5b80fec3
bjorkrothjm <j.m.bjorkroth@gmail.com>
2024-07-20 15:00:41 +0200

    Adding ejerc. de repaso

---
+++ scripts/repas.js
@@ -0,0 +1,84 @@
+
+// TODO: mostra les taules de multiplicar del 1 al 9 per la consola
+
+// TODO: recrea la funció parseFloat() de manera que agafi els números encara que hi hagi lletres abans i accepti com a separador decimal els símbols ",", "." i "'". P.e: "hola89'234" ha de tornar "89.234", "43'35adeu" ha de tornar "43.35", "amor45.9odi" ha de tornar "45.9"
+
+// TODO: crea una funció que agafi un string i que torni una lletra aleatoria (sense contar espais i signes de puntuació)
+
+// TODO: crea una funció que agafi un text i li'n separi les paraules (sense signes de puntuació) i les torni en un nou string separades per espais (sense usar arrays)
+
+// TODO: crea una funció que agafi un string i que torni una paraula aleatòria continguda en ella (sense usar arrays)
+
+// TODO: paradoxa de l'aniversari. Genera N dates d'aniversari aleatòries (1-365) en un array i comprova si hi ha alguna repetida. Fes això per N = 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 i mostra a la consola per quins hi ha hagut coincidències d'aniversari
+
+
+// TODO: dues funcions que gestionin les dades rebudes en un array d'strings o en una string amb totes juntes
+// data0 = "NOM Omar"
+// data1 = "COG Olmedo"   -> [data0, data1, data2, ...]
+// data2 = "AGE 33"
+
+// data = "NOM Omar COG Olmedo AGE 33"
+
+// La funció ha de retornar un objecte així:
+
+// {
+//     nom: "Omar",
+//     cognom: "Olmedo",
+//     edat: 33
+// }
+
+// TODO: gestionar un CSV. El programa ha d'agafar un string en format CSV i ficar tota la informació dins un array d'objectes amb els noms de les columnes com a propietats
+// TODO: manage a CSV. The program should take a sting in format CSV and add all the info into an array of objects with the names of the columns as properties
+
+// PISTA: necessitareu ajuda. Demaneu-la
+// PISTA 2: encara que no hi hagi \n per marcar els salts de línia, els detexta igual perquè uso cometes ``
+
+var csv = `Year,Make,Model,Description,Price
+1997,Ford,E350,ac abs moon,3000.00
+1999,Chevy,Venture "Extended Edition",,4900.00
+1999,Chevy,Venture "Extended Edition XL",,5000.00
+1996,Jeep,Grand Cherokee,MUST SELL! air moon-roof loaded,4799.00`
+
+console.log(csv);
+
+csv = csv.split(",")
+console.log(csv);
+
+rowLength = 5
+
+var cars = [] 
+for (let i = 0; i < csv.length; i++) {
+  for (let j = 0; j < rowLength; j++) {
+
+    class myObject {
+      constructor( )
+    }
+
+    car = {
+      csv
+
+    }
+
+
+    var[j] = car
+
+
+  }
+}
+
+// csv = $.csv.toArrays(csv)
+// console.log(csv);
+
+
+// var cotxes = [ { Year = 1997,
+//                  Make = "Ford",
+//                  Model = "E350",
+//                  Description = "ac, abs, moon",
+//                  Price = 3000.00
+//                 },    
+//                { Year = 1997,
+//                  Make = "Ford",
+//                  Model = "E350",
+//                  Description = "",
+//                  Price = 3000.00
+//                 } ];
